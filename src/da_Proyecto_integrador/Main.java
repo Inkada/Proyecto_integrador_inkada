fb202b321ca7140078a455ade18ed312d49ccacd
estudiante <estudiante@192.168.115.28>
2017-03-06 10:08:06 -0500

    importando librerias

--- src/da_Proyecto_integrador/Main.java
+++ src/da_Proyecto_integrador/Main.java
@@ -1,10 +1,16 @@
 package da_Proyecto_integrador;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+     PApplet.main("main");
+     
 	}
 
+	
+	
+	
+	
 }
