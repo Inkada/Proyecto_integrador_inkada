052976dc8d2c37bae617c40e2f11975eac61b333
estudiante <estudiante@192.168.115.28>
2017-03-06 10:21:24 -0500

    finalizacion jornada main logica

---
+++ src/da_Proyecto_integrador/Logica.java
@@ -0,0 +1,10 @@
+package da_Proyecto_integrador;
+
+import processing.core.PApplet;
+
+public class Logica {
+
+	public Logica(PApplet aap) {
+
+	}
+}
--- src/da_Proyecto_integrador/Main.java
+++ src/da_Proyecto_integrador/Main.java
@@ -2,15 +2,27 @@ package da_Proyecto_integrador;
 
 import processing.core.PApplet;
 
-public class Main extends PApplet{
+public class Main extends PApplet {
+
+	Logica log;
 
 	public static void main(String[] args) {
-     PApplet.main("main");
-     
+		PApplet.main("main");
+
 	}
 
-	
-	
-	
-	
+	public void settings() {
+		size(600, 700);
+
+	}
+
+	public void setup() {
+		log = new Logica(this);
+
+	}
+
+	public void draw() {
+
+	}
+
 }
